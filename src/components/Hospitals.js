6f2d1253a8d5bfb54fef1f1936a5cb3e5982fa49
Nishchal-007 <letscollabtogether10@gmail.com>
2021-04-25 17:02:37 +0530

    API late fetch issue solved

--- src/components/Hospitals.js
+++ src/components/Hospitals.js
@@ -1,5 +1,13 @@
-import React from "react";
-import { Container, Box, Heading, Text, SimpleGrid } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import {
+    Container,
+    Box,
+    Heading,
+    Text,
+    SimpleGrid,
+    Center,
+} from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 import axios from "axios";
 
@@ -19,63 +27,125 @@ function Feature({ title, desc, ...rest }) {
             <Text mt={4} color="white">
                 {desc}
             </Text>
+
+            <DeleteIcon
+                w={6}
+                h={6}
+                color="red.500"
+                style={{ cursor: "pointer" }}
+                marginTop={8}
+                onClick={() => alert("Delete")}
+            />
         </Box>
     );
 }
 
 const Hospitals = () => {
+    const [lt, setLt] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // async function getData() {
+        //     setLoading(true);
+        //     axios
+        //         .get("/hospitals")
+        //         .then((snap) => {
+        //             // console.log(snap.data);
+        //             setLt(snap.data);
+        //         })
+        //         .catch((err) => {
+        //             console.log("Error:", err);
+        //         });
+        // }
+        // getData();
+        setLoading(true);
+        fetch("/hospitals")
+            .then((response) => response.json())
+            .then((data) => setLt(data));
+        setLoading(false);
+    }, []);
+
+    if (!lt || lt.length === 0) {
+        return (
+            <Container centerContent padding={10}>
+                <Heading padding={10} color="white">
+                    Loading . . . .
+                </Heading>
+            </Container>
+        );
+    }
     return (
         <div>
-            <Container centerContent padding={10}>
-                <Heading color="white">Hospitals Available</Heading>
-                <Box
-                    marginTop={10}
-                    as="button"
-                    borderRadius="md"
-                    bg="tomato"
-                    color="white"
-                    px={4}
-                    h={8}
-                >
-                    Add Hospital
-                </Box>
-            </Container>
-            <SimpleGrid minChildWidth="450px" spacing="40px" padding={25}>
-                <Feature
-                    title="Plan Money"
-                    desc="The future can be even brighter but a goal without a plan is just a wish"
-                />
-                <Feature
-                    title="Save Money"
-                    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
-                />
-                <Feature
-                    title="Save Money"
-                    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
-                />
-                <Feature
-                    title="Save Money"
-                    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
-                />
-                <Feature
-                    title="Save Money"
-                    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
-                />
-                <Feature
-                    title="Save Money"
-                    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
-                />
-                <Feature
-                    title="Save Money"
-                    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
-                />
-                <Feature
-                    title="Save Money"
-                    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
-                />
-            </SimpleGrid>
+            {loading ? (
+                <Container centerContent padding={10}>
+                    <Heading padding={10} color="white">
+                        Loading . . . .
+                    </Heading>
+                </Container>
+            ) : (
+                <>
+                    <Container centerContent padding={10}>
+                        <Heading color="white">Hospitals Available</Heading>
+                        <Box
+                            marginTop={10}
+                            as="button"
+                            borderRadius="md"
+                            bg="tomato"
+                            color="white"
+                            px={4}
+                            h={8}
+                        >
+                            Add Hospital
+                        </Box>
+                    </Container>
+                    <SimpleGrid
+                        minChildWidth="450px"
+                        spacing="40px"
+                        padding={25}
+                    >
+                        {lt.map((data) => (
+                            <Feature
+                                key={data._id}
+                                title={data.name}
+                                desc={data.address}
+                            />
+                        ))}
+                    </SimpleGrid>
+                </>
+            )}
         </div>
     );
 };
 
 export default Hospitals;
+
+{
+    /* <Feature
+    title="Save Money"
+    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
+/>
+<Feature
+    title="Save Money"
+    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
+/>
+<Feature
+    title="Save Money"
+    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
+/>
+<Feature
+    title="Save Money"
+    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
+/>
+<Feature
+    title="Save Money"
+    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
+/>
+<Feature
+    title="Save Money"
+    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
+/>
+<Feature
+    title="Save Money"
+    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
+/> */
+}
