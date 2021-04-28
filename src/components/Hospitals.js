7b5b28dd5557456b522ab6b664d620d3d373730e
Nishchal-007 <letscollabtogether10@gmail.com>
2021-04-28 10:44:51 +0530

    Adding Hospitals going on

--- src/components/Hospitals.js
+++ src/components/Hospitals.js
@@ -8,6 +8,13 @@ import {
     Input,
     Button,
     Center,
+    Stack,
+    FormLabel,
+    InputGroup,
+    InputLeftAddon,
+    InputRightAddon,
+    Select,
+    Textarea,
     Drawer,
     DrawerBody,
     DrawerFooter,
@@ -25,39 +32,114 @@ const Hospitals = () => {
     const [lt, setLt] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    const [hname, setHname] = useState(null);
+    const [hadd, setHadd] = useState(null);
+    const [phone, setPhone] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const hosp = { hname, hadd, phone };
+        setLoading(true);
+    };
+
     function DrawerExample() {
         const { isOpen, onOpen, onClose } = useDisclosure();
-        const btnRef = React.useRef();
+        const firstField = React.useRef();
 
         return (
             <>
-                <Button ref={btnRef} onClick={onOpen}>
-                    Open
-                </Button>
+                <Box
+                    marginTop={10}
+                    as="button"
+                    borderRadius="md"
+                    bg="tomato"
+                    color="white"
+                    px={4}
+                    h={8}
+                    onClick={onOpen}
+                >
+                    Add Hospital
+                </Box>
+
                 <Drawer
                     isOpen={isOpen}
                     placement="right"
                     onClose={onClose}
-                    finalFocusRef={btnRef}
+                    size={"md"}
+                    initialFocusRef={firstField}
                 >
                     <DrawerOverlay>
                         <DrawerContent>
-                            <DrawerCloseButton />
-                            <DrawerHeader>Create your account</DrawerHeader>
+                            <DrawerCloseButton color="white" />
+                            <DrawerHeader borderBottomWidth="1px" color="white">
+                                Add New Hospital
+                            </DrawerHeader>
 
                             <DrawerBody>
-                                <Input placeholder="Type here..." />
+                                <form id="my-form" onSubmit={handleSubmit}>
+                                    <Stack spacing="24px">
+                                        <Box>
+                                            <FormLabel htmlFor="username">
+                                                Hospital Name
+                                            </FormLabel>
+                                            <Input
+                                                ref={firstField}
+                                                id="username"
+                                                color="white"
+                                                placeholder="E.x. Apollo Hospitals"
+                                                onChange={(e) =>
+                                                    setHname(e.target.value)
+                                                }
+                                            />
+                                        </Box>
+
+                                        <Box>
+                                            <FormLabel htmlFor="desc">
+                                                Address
+                                            </FormLabel>
+                                            <Textarea
+                                                id="desc"
+                                                color="white"
+                                                onChange={(e) =>
+                                                    setHadd(e.target.value)
+                                                }
+                                            />
+                                        </Box>
+
+                                        <Box>
+                                            <FormLabel htmlFor="username">
+                                                Contact
+                                            </FormLabel>
+                                            <Input
+                                                type="text"
+                                                id="contact"
+                                                color="white"
+                                                placeholder="E.x. 5678912340"
+                                                onChange={(e) =>
+                                                    setPhone(e.target.value)
+                                                }
+                                            />
+                                        </Box>
+                                    </Stack>
+                                </form>
                             </DrawerBody>
 
-                            <DrawerFooter>
+                            <DrawerFooter borderTopWidth="1px">
                                 <Button
                                     variant="outline"
                                     mr={3}
                                     onClick={onClose}
+                                    color="white"
                                 >
                                     Cancel
                                 </Button>
-                                <Button colorScheme="blue">Save</Button>
+                                <Button
+                                    type="submit"
+                                    colorScheme="blue"
+                                    form="my-form"
+                                >
+                                    Submit
+                                </Button>
                             </DrawerFooter>
                         </DrawerContent>
                     </DrawerOverlay>
@@ -140,17 +222,7 @@ const Hospitals = () => {
                 <>
                     <Container centerContent padding={10}>
                         <Heading color="white">Hospitals Available</Heading>
-                        <Box
-                            marginTop={10}
-                            as="button"
-                            borderRadius="md"
-                            bg="tomato"
-                            color="white"
-                            px={4}
-                            h={8}
-                        >
-                            <DrawerExample />
-                        </Box>
+                        <DrawerExample />
                     </Container>
                     <SimpleGrid
                         minChildWidth="450px"
