aaec737f02adc24d4b357fec9bb727ef65dafc41
Nishchal-007 <letscollabtogether10@gmail.com>
2021-04-28 11:55:04 +0530

    CRUD Done

---
+++ src/components/DrawerExample.js
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from "react";
+import {
+    Box,
+    Input,
+    Button,
+    Stack,
+    FormLabel,
+    Textarea,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+} from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+
+import axios from "axios";
+
+function DrawerExample() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const firstField = React.useRef();
+
+    const [hname, setHname] = useState("");
+    const [hadd, setHadd] = useState("");
+    const [phone, setPhone] = useState("");
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        var HID = "H" + Math.floor(Math.random() * 100);
+        axios
+            .post("/hospitals/add", {
+                hId: HID,
+                name: hname,
+                address: hadd,
+                contactNo: phone,
+            })
+            .then((snapshot) => {
+                console.log("Data Added", snapshot);
+            })
+            .catch((err) => console.log(err));
+    };
+
+    return (
+        <>
+            <Box
+                marginTop={10}
+                as="button"
+                borderRadius="md"
+                bg="tomato"
+                color="white"
+                px={4}
+                h={8}
+                onClick={onOpen}
+            >
+                Add Hospital
+            </Box>
+
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                size={"md"}
+                initialFocusRef={firstField}
+            >
+                <DrawerOverlay>
+                    <DrawerContent>
+                        <DrawerCloseButton color="white" />
+                        <DrawerHeader borderBottomWidth="1px" color="white">
+                            Add New Hospital
+                        </DrawerHeader>
+
+                        <DrawerBody>
+                            <form id="my-form" onSubmit={handleSubmit}>
+                                <Stack spacing="24px">
+                                    <Box>
+                                        <FormLabel htmlFor="username">
+                                            Hospital Name
+                                        </FormLabel>
+                                        <Input
+                                            id="username"
+                                            color="white"
+                                            placeholder="E.x. Apollo Hospitals"
+                                            onChange={(e) =>
+                                                setHname(e.target.value)
+                                            }
+                                        />
+                                    </Box>
+
+                                    <Box>
+                                        <FormLabel htmlFor="desc">
+                                            Address
+                                        </FormLabel>
+                                        <Textarea
+                                            id="desc"
+                                            color="white"
+                                            onChange={(e) =>
+                                                setHadd(e.target.value)
+                                            }
+                                        />
+                                    </Box>
+
+                                    <Box>
+                                        <FormLabel htmlFor="username">
+                                            Contact
+                                        </FormLabel>
+                                        <Input
+                                            type="text"
+                                            id="contact"
+                                            color="white"
+                                            placeholder="E.x. 5678912340"
+                                            onChange={(e) =>
+                                                setPhone(e.target.value)
+                                            }
+                                        />
+                                    </Box>
+                                </Stack>
+                            </form>
+                        </DrawerBody>
+
+                        <DrawerFooter borderTopWidth="1px">
+                            <Button
+                                variant="outline"
+                                mr={3}
+                                onClick={onClose}
+                                color="white"
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                type="submit"
+                                colorScheme="blue"
+                                form="my-form"
+                            >
+                                Submit
+                            </Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        </>
+    );
+}
+
+export default DrawerExample;
--- src/components/Hospitals.js
+++ src/components/Hospitals.js
@@ -1,30 +1,7 @@
 import React, { useState, useEffect } from "react";
-import {
-    Container,
-    Box,
-    Heading,
-    Text,
-    SimpleGrid,
-    Input,
-    Button,
-    Center,
-    Stack,
-    FormLabel,
-    InputGroup,
-    InputLeftAddon,
-    InputRightAddon,
-    Select,
-    Textarea,
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    useDisclosure,
-} from "@chakra-ui/react";
+import { Container, Box, Heading, Text, SimpleGrid } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
+import DrawerExample from "./DrawerExample";
 
 import axios from "axios";
 
@@ -32,123 +9,7 @@ const Hospitals = () => {
     const [lt, setLt] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const [hname, setHname] = useState(null);
-    const [hadd, setHadd] = useState(null);
-    const [phone, setPhone] = useState(null);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const hosp = { hname, hadd, phone };
-        setLoading(true);
-    };
-
-    function DrawerExample() {
-        const { isOpen, onOpen, onClose } = useDisclosure();
-        const firstField = React.useRef();
-
-        return (
-            <>
-                <Box
-                    marginTop={10}
-                    as="button"
-                    borderRadius="md"
-                    bg="tomato"
-                    color="white"
-                    px={4}
-                    h={8}
-                    onClick={onOpen}
-                >
-                    Add Hospital
-                </Box>
-
-                <Drawer
-                    isOpen={isOpen}
-                    placement="right"
-                    onClose={onClose}
-                    size={"md"}
-                    initialFocusRef={firstField}
-                >
-                    <DrawerOverlay>
-                        <DrawerContent>
-                            <DrawerCloseButton color="white" />
-                            <DrawerHeader borderBottomWidth="1px" color="white">
-                                Add New Hospital
-                            </DrawerHeader>
-
-                            <DrawerBody>
-                                <form id="my-form" onSubmit={handleSubmit}>
-                                    <Stack spacing="24px">
-                                        <Box>
-                                            <FormLabel htmlFor="username">
-                                                Hospital Name
-                                            </FormLabel>
-                                            <Input
-                                                ref={firstField}
-                                                id="username"
-                                                color="white"
-                                                placeholder="E.x. Apollo Hospitals"
-                                                onChange={(e) =>
-                                                    setHname(e.target.value)
-                                                }
-                                            />
-                                        </Box>
-
-                                        <Box>
-                                            <FormLabel htmlFor="desc">
-                                                Address
-                                            </FormLabel>
-                                            <Textarea
-                                                id="desc"
-                                                color="white"
-                                                onChange={(e) =>
-                                                    setHadd(e.target.value)
-                                                }
-                                            />
-                                        </Box>
-
-                                        <Box>
-                                            <FormLabel htmlFor="username">
-                                                Contact
-                                            </FormLabel>
-                                            <Input
-                                                type="text"
-                                                id="contact"
-                                                color="white"
-                                                placeholder="E.x. 5678912340"
-                                                onChange={(e) =>
-                                                    setPhone(e.target.value)
-                                                }
-                                            />
-                                        </Box>
-                                    </Stack>
-                                </form>
-                            </DrawerBody>
-
-                            <DrawerFooter borderTopWidth="1px">
-                                <Button
-                                    variant="outline"
-                                    mr={3}
-                                    onClick={onClose}
-                                    color="white"
-                                >
-                                    Cancel
-                                </Button>
-                                <Button
-                                    type="submit"
-                                    colorScheme="blue"
-                                    form="my-form"
-                                >
-                                    Submit
-                                </Button>
-                            </DrawerFooter>
-                        </DrawerContent>
-                    </DrawerOverlay>
-                </Drawer>
-            </>
-        );
-    }
-
-    function Feature({ name, address, location, contact, id, ...rest }) {
+    function Feature({ name, address, contact, id, ...rest }) {
         return (
             <Box
                 p={5}
@@ -163,9 +24,6 @@ const Hospitals = () => {
                 </Heading>
                 <Text mt={4} color="white">
                     Address: {address}
-                </Text>
-                <Text mt={4} color="white">
-                    Co-ordinates: {location}
                 </Text>
                 <Text mt={4} color="white">
                     Contact: {contact}
@@ -234,7 +92,6 @@ const Hospitals = () => {
                                 key={data._id}
                                 name={data.name}
                                 address={data.address}
-                                location={data.location}
                                 contact={data.contactNo}
                                 id={data.hId}
                             />
